67154bbe11b4acfbec1c6ee02db3cfba316e93de
Lucas Menendez <epucas@gmail.com>
2021-12-21 14:51:23 +0100

    Logger class comments

--- workflow/training_monitoring/src/logger.py
+++ workflow/training_monitoring/src/logger.py
@@ -6,8 +6,11 @@ import requests
 
 class Logger:
     def __init__(self, job_id, node_id):
+        # store prometheus job and node ID
         self.job_id = job_id
         self.node_id = node_id
+
+        # format the pushgateway URI 
         self.uri = 'http://logger:9091/metrics/job/%s/node/%s' % (job_id, node_id)
 
     def log(self, metric: str, value: Number, extra_labels: Dict = None):
@@ -24,21 +27,29 @@ class Logger:
             response = requests.post(self.uri, data=data)
             response.raise_for_status()
         except Exception as err:
+            # print generic exception
             print(err)
+
+            # print complex exception when it is a HTTP exception
             with err.response as res:
                 print("[Logger status %d]: %s (%s -> %d %s %s)" % (res.status_code, 
                     res.text, metric, value, labels, self.uri))
     
     def logExecutionTime(self, method):
+        # create a decorator to measure the execution time of any function
         def inner(*args, **kwargs):
+            # get the initial time, execute the decorated method and get the 
+            # result and the final time.
             start_time = time.time()
             res = method(*args, **kwargs)
             end_time = time.time()
             
+            # calc elapsed time in seconds, register the trace and return 
+            # the decorated result.
             execution_time = (end_time - start_time)
             print(execution_time)
             self.log("method_duration_seconds", execution_time, { "method_name": method.__name__ })
-            
             return res
 
+        # return inner decoration function
         return inner
